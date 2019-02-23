4b1da916f433addd9f79255e6ef97f5e2b7e6ad8
Julien Tschäppät <git@elendil.software>
2019-02-23 08:44:24 +0100

    Test app for ObservingConditions drivers

---
+++ ASCOM.ObservingConditions.TestApplication/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace ASCOM.YoctoMeteo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonChoose = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.labelDriverId = new System.Windows.Forms.Label();
+            this.GetDataButton = new System.Windows.Forms.Button();
+            this.TemperatureLabel = new System.Windows.Forms.Label();
+            this.HumidityLabel = new System.Windows.Forms.Label();
+            this.PressureLabel = new System.Windows.Forms.Label();
+            this.DewPointLabel = new System.Windows.Forms.Label();
+            this.DewPointValue = new System.Windows.Forms.Label();
+            this.PressureValue = new System.Windows.Forms.Label();
+            this.HumidityValue = new System.Windows.Forms.Label();
+            this.TemperatureValue = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonChoose
+            // 
+            this.buttonChoose.Location = new System.Drawing.Point(309, 10);
+            this.buttonChoose.Name = "buttonChoose";
+            this.buttonChoose.Size = new System.Drawing.Size(72, 23);
+            this.buttonChoose.TabIndex = 0;
+            this.buttonChoose.Text = "Choose";
+            this.buttonChoose.UseVisualStyleBackColor = true;
+            this.buttonChoose.Click += new System.EventHandler(this.buttonChoose_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(309, 39);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(72, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // labelDriverId
+            // 
+            this.labelDriverId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDriverId.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::ASCOM.YoctoMeteo.Properties.Settings.Default, "DriverId", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.labelDriverId.Location = new System.Drawing.Point(12, 40);
+            this.labelDriverId.Name = "labelDriverId";
+            this.labelDriverId.Size = new System.Drawing.Size(291, 21);
+            this.labelDriverId.TabIndex = 2;
+            this.labelDriverId.Text = global::ASCOM.YoctoMeteo.Properties.Settings.Default.DriverId;
+            this.labelDriverId.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // GetDataButton
+            // 
+            this.GetDataButton.Enabled = false;
+            this.GetDataButton.Location = new System.Drawing.Point(12, 76);
+            this.GetDataButton.Name = "GetDataButton";
+            this.GetDataButton.Size = new System.Drawing.Size(75, 23);
+            this.GetDataButton.TabIndex = 3;
+            this.GetDataButton.Text = "Get Data";
+            this.GetDataButton.UseVisualStyleBackColor = true;
+            this.GetDataButton.Click += new System.EventHandler(this.GetDataButton_Click);
+            // 
+            // TemperatureLabel
+            // 
+            this.TemperatureLabel.AutoSize = true;
+            this.TemperatureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TemperatureLabel.Location = new System.Drawing.Point(12, 113);
+            this.TemperatureLabel.Name = "TemperatureLabel";
+            this.TemperatureLabel.Size = new System.Drawing.Size(78, 13);
+            this.TemperatureLabel.TabIndex = 4;
+            this.TemperatureLabel.Text = "Temperature";
+            // 
+            // HumidityLabel
+            // 
+            this.HumidityLabel.AutoSize = true;
+            this.HumidityLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HumidityLabel.Location = new System.Drawing.Point(12, 138);
+            this.HumidityLabel.Name = "HumidityLabel";
+            this.HumidityLabel.Size = new System.Drawing.Size(55, 13);
+            this.HumidityLabel.TabIndex = 8;
+            this.HumidityLabel.Text = "Humidity";
+            // 
+            // PressureLabel
+            // 
+            this.PressureLabel.AutoSize = true;
+            this.PressureLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PressureLabel.Location = new System.Drawing.Point(12, 163);
+            this.PressureLabel.Name = "PressureLabel";
+            this.PressureLabel.Size = new System.Drawing.Size(56, 13);
+            this.PressureLabel.TabIndex = 9;
+            this.PressureLabel.Text = "Pressure";
+            // 
+            // DewPointLabel
+            // 
+            this.DewPointLabel.AutoSize = true;
+            this.DewPointLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DewPointLabel.Location = new System.Drawing.Point(12, 188);
+            this.DewPointLabel.Name = "DewPointLabel";
+            this.DewPointLabel.Size = new System.Drawing.Size(61, 13);
+            this.DewPointLabel.TabIndex = 10;
+            this.DewPointLabel.Text = "DewPoint";
+            // 
+            // DewPointValue
+            // 
+            this.DewPointValue.AutoSize = true;
+            this.DewPointValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DewPointValue.Location = new System.Drawing.Point(109, 188);
+            this.DewPointValue.Name = "DewPointValue";
+            this.DewPointValue.Size = new System.Drawing.Size(53, 13);
+            this.DewPointValue.TabIndex = 14;
+            this.DewPointValue.Text = "DewPoint";
+            // 
+            // PressureValue
+            // 
+            this.PressureValue.AutoSize = true;
+            this.PressureValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PressureValue.Location = new System.Drawing.Point(109, 163);
+            this.PressureValue.Name = "PressureValue";
+            this.PressureValue.Size = new System.Drawing.Size(48, 13);
+            this.PressureValue.TabIndex = 13;
+            this.PressureValue.Text = "Pressure";
+            // 
+            // HumidityValue
+            // 
+            this.HumidityValue.AutoSize = true;
+            this.HumidityValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HumidityValue.Location = new System.Drawing.Point(109, 138);
+            this.HumidityValue.Name = "HumidityValue";
+            this.HumidityValue.Size = new System.Drawing.Size(47, 13);
+            this.HumidityValue.TabIndex = 12;
+            this.HumidityValue.Text = "Humidity";
+            // 
+            // TemperatureValue
+            // 
+            this.TemperatureValue.AutoSize = true;
+            this.TemperatureValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TemperatureValue.Location = new System.Drawing.Point(109, 113);
+            this.TemperatureValue.Name = "TemperatureValue";
+            this.TemperatureValue.Size = new System.Drawing.Size(67, 13);
+            this.TemperatureValue.TabIndex = 11;
+            this.TemperatureValue.Text = "Temperature";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 262);
+            this.Controls.Add(this.DewPointValue);
+            this.Controls.Add(this.PressureValue);
+            this.Controls.Add(this.HumidityValue);
+            this.Controls.Add(this.TemperatureValue);
+            this.Controls.Add(this.DewPointLabel);
+            this.Controls.Add(this.PressureLabel);
+            this.Controls.Add(this.HumidityLabel);
+            this.Controls.Add(this.TemperatureLabel);
+            this.Controls.Add(this.GetDataButton);
+            this.Controls.Add(this.labelDriverId);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.buttonChoose);
+            this.Name = "Form1";
+            this.Text = "TEMPLATEDEVICETYPE Test";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChoose;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Label labelDriverId;
+        private System.Windows.Forms.Button GetDataButton;
+        private System.Windows.Forms.Label TemperatureLabel;
+        private System.Windows.Forms.Label HumidityLabel;
+        private System.Windows.Forms.Label PressureLabel;
+        private System.Windows.Forms.Label DewPointLabel;
+        private System.Windows.Forms.Label DewPointValue;
+        private System.Windows.Forms.Label PressureValue;
+        private System.Windows.Forms.Label HumidityValue;
+        private System.Windows.Forms.Label TemperatureValue;
+    }
+}
+
---
+++ ASCOM.ObservingConditions.TestApplication/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Forms;
+
+namespace ASCOM.YoctoMeteo
+{
+    public partial class Form1 : Form
+    {
+
+        private ASCOM.DriverAccess.ObservingConditions driver;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetUIState();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (IsConnected)
+                driver.Connected = false;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void buttonChoose_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.DriverId = ASCOM.DriverAccess.ObservingConditions.Choose(Properties.Settings.Default.DriverId);
+            SetUIState();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (IsConnected)
+            {
+                driver.Connected = false;
+                GetDataButton.Enabled = false;
+
+            }
+            else
+            {
+                try
+                {
+                    driver = new ASCOM.DriverAccess.ObservingConditions(Properties.Settings.Default.DriverId);
+                    driver.Connected = true;
+                    GetDataButton.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            SetUIState();
+        }
+
+        private void SetUIState()
+        {
+            buttonConnect.Enabled = !string.IsNullOrEmpty(Properties.Settings.Default.DriverId);
+            buttonChoose.Enabled = !IsConnected;
+            buttonConnect.Text = IsConnected ? "Disconnect" : "Connect";
+        }
+
+        private bool IsConnected
+        {
+            get
+            {
+                return ((this.driver != null) && (driver.Connected == true));
+            }
+        }
+
+        private void GetDataButton_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                TemperatureValue.Text = driver.Temperature.ToString(CultureInfo.InvariantCulture);
+                PressureValue.Text = driver.Pressure.ToString(CultureInfo.InvariantCulture);
+                HumidityValue.Text = driver.Humidity.ToString(CultureInfo.InvariantCulture);
+                DewPointValue.Text = driver.DewPoint.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
